14cec7c34099d6b39c6a24827b4d5fa141b97971
Artem Golovin <artgolovin96@gmail.com>
2019-12-02 04:07:35 -0700

    :sparkles: (p2) train the model to be 93% accurate

--- p2/notMNISTStarter.py
+++ p2/notMNISTStarter.py
@@ -1,34 +1,42 @@
-# For Google Collab
-# try:
-#  # %tensorflow_version only exists in Colab.
-#  %tensorflow_version 2.x
-# except Exception:
-#  pass
-
 import tensorflow as tf
 import numpy as np
+import os
 
-print("--Get data--")
-with np.load("notMNIST.npz", allow_pickle=True) as f:
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
 
-print("--Process data--")
-print(len(y_train))
-x_train, x_test = x_train / 255.0, x_test / 255.0
+def train():
+    print("--Get data--")
+    with np.load(f'{os.getcwd()}/notMNIST.npz', allow_pickle=True) as f:
+        x_train, y_train = f['x_train'], f['y_train']
+        x_test, y_test = f['x_test'], f['y_test']
 
-print("--Make model--")
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(10, activation='sigmoid')
-])
-model.compile(optimizer='sgd',
-              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    print("--Process data--")
+    print(len(y_train))
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
-print("--Fit model--")
-model.fit(x_train, y_train, epochs=1, verbose=2)
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adamax',
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-print("--Evaluate model--")
-model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
-print(f"Model Loss:    {model_loss:.2f}")
-print(f"Model Accuray: {model_acc*100:.1f}%")
+    print("--Fit model--")
+    model.fit(x_train, y_train, epochs=10, verbose=2)
+
+    print("--Evaluate model--")
+    model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
+    print(f"Model Loss:    {model_loss:.2f}")
+    print(f"Model Accuray: {model_acc*100:.1f}%")
+
+    return model
+
+
+def main():
+    model = train()
+    model.save(f'{os.getcwd()}/notMNIST.h5')
+
+
+if __name__ == "__main__":
+    main()
