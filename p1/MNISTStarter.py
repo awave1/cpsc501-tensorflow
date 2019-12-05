cef36b02b1945f6097d837b36302e513eae996ac
awave1 <artgolovin96@gmail.com>
2019-12-04 21:44:31 -0700

    :zap: (p1) improve accuracy of MNIST model

--- p1/MNISTStarter.py
+++ p1/MNISTStarter.py
@@ -12,26 +12,18 @@ def train():
     print("--Make model--")
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(512, activation='relu'),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10, activation='softmax')
     ])
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    print("--Fit test model--")
-    model.fit(x_test, y_test, epochs=10, verbose=2)
-
-    print("--Fit train model--")
+    print("--Fit model--")
     model.fit(x_train, y_train, epochs=10, verbose=2)
 
-    print("--Evaluate test model--")
+    print("--Evaluate model--")
     model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
-    print(f"Model Loss:    {model_loss:.2f}")
-    print(f"Model Accuray: {model_acc*100:.1f}%")
-
-    print("--Evaluate train model--")
-    model_loss, model_acc = model.evaluate(x_train,  y_train, verbose=2)
     print(f"Model Loss:    {model_loss:.2f}")
     print(f"Model Accuray: {model_acc*100:.1f}%")
 
